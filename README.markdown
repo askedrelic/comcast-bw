b0c7f62145da5ff2179a8ae01559dbfcaf44eaad
Matt Behrens <askedrelic@gmail.com>
2010-10-23 16:56:14 -0400

    readme, cleanup, and object orientedness

---
+++ README.markdown
@@ -0,0 +1,20 @@
+Comcast Bandwidth
+=================
+
+As the repo states, it's a script to login to comcast's website and find your current bandwidth usage. I hate using Comcast's website and I haven't seem to found any automatic notifications for when you come near your 250GB limit.
+
+Now you can setup a cronjob and get daily/weekly stats emailed to you.
+
+Requirements
+-----
+[mechanize library](http://wwwsearch.sourceforge.net/mechanize/)
+
+Usage
+-----
+You need to edit the SETTINGS at the top of the file to insert your comcast username/password.
+
+Broken?
+-------
+I've tested this with my username/account. Comcast's website makes extensive use of "preloading" and redirects, which are stupid and why I hate using it, but I may not have caught every possible combination. 
+
+If you run the script with '-v' flag, you can get debug mode in mechanize and python logging to figure out where it's broken.
--- comcast-bw.py
+++ comcast-bw.py
@@ -8,91 +8,118 @@ Matt Behrens <askedrelic@gmail.com> http://asktherelic.com
 Script to login to comcast's website and find your current bandwidth usage.
 """
 
+import sys
 import urlparse
 import cookielib
 import warnings
 import logging
+import getopt
 
 import mechanize
 
 ## SETTINGS
-username = "COMCAST USERNAME"
+username = "COMCAST USERNAME (username@comcast.net)"
 password = "COMCAST PASSWORD"
 
-# Browser
-br = mechanize.Browser()
+class Comcast(object):
+    def __init__(self, verbose, username, password):
+        self.username = username
+        self.password = password
+        self.verbose = verbose
 
-# Cookie Jar
-cj = cookielib.LWPCookieJar()
-br.set_cookiejar(cj)
+    def currentUsage(self):
+        # Browser
+        br = mechanize.Browser()
 
-# Logger
-log = logging.getLogger(__name__)
+        # Cookie Jar
+        cj = cookielib.LWPCookieJar()
+        br.set_cookiejar(cj)
 
-# Browser options
-br.set_handle_equiv(True)
-with warnings.catch_warnings():
-    warnings.simplefilter("ignore")
-    br.set_handle_gzip(True)
-br.set_handle_redirect(True)
-br.set_handle_referer(True)
-br.set_handle_robots(False)
+        # Logger
+        log = logging.getLogger(__name__)
 
-# Follows refresh 0 but not hangs on refresh > 0
-br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
+        # Browser options
+        br.set_handle_equiv(True)
+        with warnings.catch_warnings():
+            warnings.simplefilter("ignore")
+            br.set_handle_gzip(True)
+        br.set_handle_redirect(True)
+        br.set_handle_referer(True)
+        br.set_handle_robots(False)
 
-# Want debugging messages?
-#br.set_debug_http(True)
-#br.set_debug_redirects(True)
-#br.set_debug_responses(True)
-#console = logging.StreamHandler()
-#log.addHandler(console)
-#log.setLevel(logging.INFO)
+        # Follows refresh 0 but not hangs on refresh > 0
+        br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
-# Set a reasonably current User-Agent
-br.addheaders = [('User-agent', 'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.11 Safari/534.10')]
+        # Want debugging messages?
+        if self.verbose:
+            br.set_debug_http(True)
+            br.set_debug_redirects(True)
+            br.set_debug_responses(True)
+            console = logging.StreamHandler()
+            log.addHandler(console)
+            log.setLevel(logging.INFO)
 
-# Head to Login page
-br.open('https://customer.comcast.com/Public/Home.aspx')
-log.info('Loading sign in page')
-sign_link = br.find_link(text="Sign In")
-br.follow_link(sign_link)
-br.select_form(nr=0)
-br["user"] = username
-br["passwd"] = password
-br.submit()
+        # Set a reasonably current User-Agent
+        br.addheaders = [('User-agent',
+            'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10_6_4; en-US) AppleWebKit/534.10 (KHTML, like Gecko) Chrome/8.0.552.11 Safari/534.10')]
 
-# Head to customer homepage, resubmit to get past preloader page
-log.info('Loading customer homepage, redirect')
-br.select_form(nr=0)
-br.submit()
+        # Head to Login page
+        br.open('https://customer.comcast.com/Public/Home.aspx')
+        log.info('====')
+        log.info('Loading sign in page')
+        sign_link = br.find_link(text="Sign In")
+        br.follow_link(sign_link)
+        br.select_form(nr=0)
+        br["user"] = self.username
+        br["passwd"] = self.password
+        br.submit()
 
-# Head to User information page
-br.open('https://customer.comcast.com/Secure/Users.aspx')
-log.info('Loading customer homepage')
+        # Head to customer homepage, resubmit to get past preloader page
+        log.info('====')
+        log.info('Loading customer homepage, redirect')
+        br.select_form(nr=0)
+        br.submit()
 
-# Head to 
-br.find_link(text="View details")
-log.info('loading details page')
-details = br.find_link(text="View details")
-resp = br.follow_link(details)
+        # Head to User information page
+        br.open('https://customer.comcast.com/Secure/Users.aspx')
+        log.info('====')
+        log.info('Loading customer homepage')
 
-details_link = resp.geturl()
+        # Head to 
+        br.find_link(text="View details")
+        log.info('====')
+        log.info('loading details page')
+        details = br.find_link(text="View details")
+        resp = br.follow_link(details)
 
-# Check if link is a preload page and actually follow the preload url,
-# because this preload url seems unique/generated
-if(details_link.lower().find('preload') != -1):
-    l = urlparse.urlparse(details_link)
-    url = urlparse.parse_qsl(l.query)[0][1]
-    link = urlparse.urljoin(l.geturl(), url)
+        details_link = resp.geturl()
 
-resp = br.open(details_link)
-log.info('Loaded details page')
-details_page = resp.read()
-bandwidthDiv = 'UsedWrapper">'
-start = details_page.find(bandwidthDiv) + len(bandwidthDiv)
-end = details_page.find('<', start)
+        # Check if link is a preload page and actually follow the preload url,
+        # because this preload url seems unique/generated
+        if(details_link.lower().find('preload') != -1):
+            l = urlparse.urlparse(details_link)
+            url = urlparse.parse_qsl(l.query)[0][1]
+            link = urlparse.urljoin(l.geturl(), url)
 
-used_bandwidth = details_page[start:end]
+        resp = br.open(details_link)
+        log.info('====')
+        log.info('Loaded details page')
+        details_page = resp.read()
+        bandwidthDiv = 'UsedWrapper">'
+        start = details_page.find(bandwidthDiv) + len(bandwidthDiv)
+        end = details_page.find('<', start)
 
-print "You have used %s bandwidth this month" % used_bandwidth
+        used_bandwidth = details_page[start:end]
+
+        return used_bandwidth
+
+if __name__ == '__main__':
+    verbose = False
+    # Get command line options and args
+    opts, args = getopt.getopt(sys.argv[1:], 'v')
+    for o, a in opts:
+        if o == '-v':
+            verbose = True
+
+    comcast = Comcast(verbose,username,password)
+    print "You have used %s bandwidth this month" % comcast.currentUsage()
